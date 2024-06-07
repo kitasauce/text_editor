c8cdea841d76c2b844aefc5ffbb573522c5a3838
andrew <cole.zubac@gmail.com>
2024-06-06 21:19:41 -0400

    js changes db loading

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'eadwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ content });
+  await tx.done;
+  console.log('Content added to database');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'eadonly');
+  const store = tx.objectStore('jate');
+  const data = await store.getAll();
+  await tx.done;
+  console.log("data read from database");
+  return data.map((item) => item.content);
+}
 
 initdb();
