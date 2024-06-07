b0f402ea9ce24359412f053ff46e1f38f99b2bfb
andrew <cole.zubac@gmail.com>
2024-06-06 21:49:59 -0400

    changes to db file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'eadwrite');
+  const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   await store.put({ content });
   await tx.done;
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'eadonly');
+  const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const data = await store.getAll();
   await tx.done;
